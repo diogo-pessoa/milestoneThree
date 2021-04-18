f2b26482cbfafa41e5bb92c7919e7e8a43188203
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-04-18 21:33:53 +0100

    Adding handler for register route
    - routes.py
      - including statements to handle register form
      - check if user already exists
      - handle data from form and pass it to data functions
    
    - models.py
      - created new 'insert_new_user'
      - find_user_by_name added 'if' to confirm user is not None before return
    - updating comments on register form

--- app/models.py
+++ app/models.py
@@ -1,16 +1,34 @@
+from werkzeug.security import generate_password_hash
+
 from app import mongo
 
 
 class Data(object):
-
+    # TODO insert try
     def find_all_books(self):
         return mongo.db.books.find()
-
 
     def find_all_reviews(self):
         reviews = mongo.db.reviews.find()
         return reviews
 
-    def find_user_by_name(self, name):
-        user = mongo.db.users.find_one({"username": name})
-        return user.get("username")
+    def find_user_by_name(self, name: str):
+        try:
+            user = mongo.db.users.find_one({"username": name.lower()})
+            if user:
+                return user.get("username")
+
+        except IOError as e:
+            errno, strerror = e.args
+            print(f"ERROR - Failed to get user, {errno}: {strerror}.")
+
+    def insert_new_user(self, username, password):
+        user_details = {
+            "username": username.lower(),
+            "password": generate_password_hash(password)
+        }
+        try:
+            mongo.db.users.insert_one(user_details)
+        except Exception as e:
+            errno, strerror = e.args
+            print(f"ERROR - Failed to Insert user:, {errno}: {strerror}.")
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,5 @@
-from flask import render_template
+from flask import (render_template, request,
+                   flash, redirect, url_for, session)
 from app import app
 from app.models import Data
 
@@ -14,4 +15,16 @@ def index():
 
 @app.route("/register", methods=["GET", "POST"])
 def register():
+    if request.method == "POST":
+        username = request.form.get("username")
+        password = request.form.get("password")
+        existing_user = Data().find_user_by_name(username)
+        if existing_user:
+            flash("username already in use")
+            return redirect(url_for("register"))
+
+        # Insert new user
+        Data().insert_new_user(username,
+                               password)
+        flash("Registration Successful!")
     return render_template("register.html")
