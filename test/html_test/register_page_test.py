99ac3bc6d8432bed5cbdce75e1ef2abbe52d677a
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-04-21 19:43:00 +0100

    Adding wait condition to register_page_test.py
    Test form tries to register existing user and confirm flash message shows

--- test/html_test/register_page_test.py
+++ test/html_test/register_page_test.py
@@ -1,15 +1,15 @@
 import unittest
 
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
 
 
 class RegisterPage(unittest.TestCase):
 
     def setUp(self):
         self.driver = webdriver.Firefox()
-
-    def tearDown(self):
-        self.driver.close()
 
     def test_register_page(self):
         driver = self.driver
@@ -24,7 +24,12 @@ class RegisterPage(unittest.TestCase):
         username.send_keys("NewUser")
         password = driver.find_element_by_id("password")
         password.send_keys("123")
-        driver.find_element_by_tag_name('button')
+        driver.find_element_by_tag_name('button').click()
+        WebDriverWait(driver, 3).until(
+            expected_conditions.text_to_be_present_in_element(
+                (By.TAG_NAME, "h4"), "username already in use")
+        )
+        driver.close()
 
 
 if __name__ == "__main__":
