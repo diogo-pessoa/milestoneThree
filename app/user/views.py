c121b6525e9bdbaefb83457aaed9f9b2c559f843
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-05-03 12:56:25 +0100

    Adding Reviews to reviews tab in Profile Page
    - Passing reviews list to view render_template() method
    - Looping through list and creating list of reviews

--- app/user/views.py
+++ app/user/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
 
+from app.model.review_model import ReviewModel
 from app.model.user_model import UserModel
 from src.bookshelf.user import User
 
@@ -10,4 +11,5 @@ user = Blueprint('user', __name__, template_folder='templates')
 def profile(user):
     user_information = UserModel().find_user_by_name(user)
     loaded_user = User(user_information)
-    return render_template("profile.html", user=loaded_user)
+    reviews = ReviewModel().find_all_user_reviews(loaded_user.get_username())
+    return render_template("profile.html", user=loaded_user, reviews=reviews)
