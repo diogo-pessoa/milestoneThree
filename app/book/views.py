7fe1223ad18b35e652d722c183a6cc549c547309
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-05-04 23:20:39 +0100

    Adding Book html template
    - new find_book_by_title() on Book Model
    - New Book.html template for book view
    - book view adding find_book_by_title() to book route

--- app/book/views.py
+++ app/book/views.py
@@ -5,8 +5,7 @@ from app.model.book_model import BookModel
 book = Blueprint('book', __name__, template_folder='templates')
 
 
-@book.route('/')
-@book.route('/books')
+@book.route('/book')
 def book_list():
     books_list = BookModel().find_all_books()
     return render_template('books.html', books=books_list)
@@ -14,4 +13,5 @@ def book_list():
 
 @book.route("/book/<book_name>", methods=["GET", "POST"])
 def book_page(book_name):
-    return render_template('book.html', book=book_name)
+    single_book = BookModel().find_book_by_title(book_name)
+    return render_template('book.html', book=single_book)
--- app/model/book_model.py
+++ app/model/book_model.py
@@ -9,8 +9,28 @@ class BookModel(object):
         Returns a list with all Books from Books collections
         :return: list of Book objects from DB
         """
-        books = []
-        books_from_db = mongo.db.books.find()
-        for book in books_from_db:
-            books.append(Book(book))
-        return books
+        try:
+            books = []
+            books_from_db = mongo.db.books.find()
+            for book in books_from_db:
+                books.append(Book(book))
+            return books
+
+        except IOError as e:
+            errno, strerror = e.args
+            print(f"ERROR - Failed to get user, {errno}: {strerror}.")
+
+    def find_book_by_title(self, book_name: str):
+        """
+        Find book by title
+        :param book_name: single book_name
+        :return: Book object
+        """
+
+        try:
+            single_book = mongo.db.books.find_one({"title": book_name})
+            if single_book:
+                return Book(single_book)
+        except IOError as e:
+            errno, strerror = e.args
+            print(f"ERROR - Failed to get user, {errno}: {strerror}.")
