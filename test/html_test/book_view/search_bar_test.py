92df61dbee50db95e6f28bb73ace3eecf079cf38
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-06-28 21:51:53 +0100

    Search Bar test on staging environment

--- test/html_test/book_view/search_bar_test.py
+++ test/html_test/book_view/search_bar_test.py
@@ -12,13 +12,13 @@ class SearchBar(unittest.TestCase):
         options = webdriver.FirefoxOptions()
         options.add_argument('')
         self.driver = webdriver.Firefox(options=options)
-        self.driver.get("http://localhost:5000/book")
+        self.driver.get("https://the-bookshelf-milestone-three.herokuapp.com/book")
 
     def tearDown(self):
         self.driver.close()
 
     def login(self):
-        self.driver.get("http://0.0.0.0:5000/login")
+        self.driver.get("https://the-bookshelf-milestone-three.herokuapp.com/login")
         username_tested = self.driver.find_element_by_id("username")
         username_tested.send_keys("willfarnaby")
         password_tested = self.driver.find_element_by_id("password")
@@ -35,20 +35,19 @@ class SearchBar(unittest.TestCase):
         self.assertEqual("RESET", reset_button_text)
         self.assertEqual("SEARCH", search_button_text)
 
-    def test_search_for_book_1(self):
+    def test_search_for_book(self):
         book_title = self.driver.find_element_by_id("query")
-        book_title.send_keys("Book 12")
+        book_title.send_keys("How To Kill A Mockingbird")
         self.driver.find_element_by_tag_name('button').click()
         WebDriverWait(self.driver, 3).until(
             expected_conditions.text_to_be_present_in_element(
-                (By.CLASS_NAME, "title"), "Book 12")
+                (By.CLASS_NAME, "title"), "How To Kill A Mockingbird")
         )
 
     def test_search_no_matching_result(self):
         """
             Runs a search for with a book that doesn't exist,
             response should inform no book was found and redirect user to register if not logged in
-        :return:
         """
         book_title = self.driver.find_element_by_id("query")
         book_title.send_keys("Nothing to be found")
@@ -63,9 +62,8 @@ class SearchBar(unittest.TestCase):
 
     def test_search_no_matching_result_logged_in(self):
         """
-            Runs a search for with a book that doesn't exist,
-            response should inform no book was found and redirect user to create new book
-            :return:
+            Runs a search for a book that doesn't exist,
+            response should inform no book was not found and redirect user to create new book
         """
         # Login
         self.login()
@@ -75,12 +73,15 @@ class SearchBar(unittest.TestCase):
         self.driver.find_element_by_tag_name('button').click()
         # click on create new book link
         self.driver.find_element_by_link_text('Create new book').click()
-        WebDriverWait(self.driver, 3).until(
-            expected_conditions.text_to_be_present_in_element(
-                (By.TAG_NAME, "h5"), "About This Book")
-        )
+        # Checks if new book page loads, and sets the submitted by value to current user
+        created_by = self.driver.find_element_by_id("created_by").get_property('value')
+        self.assertEqual("willfarnaby", created_by)
 
     def test_search_by_author(self):
+        """
+            Runs a search for book by author
+            response should show results for authors matching query
+        """
         book_title = self.driver.find_element_by_id("query")
         book_title.send_keys("Jon Doe")
         self.driver.find_element_by_tag_name('button').click()
