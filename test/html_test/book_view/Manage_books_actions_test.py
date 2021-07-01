460e5016da15a8711ff5effb7824489e46c633b8
Diogo Pessoa <diogo.santos.pessoa@gmail.com>
2021-07-01 21:41:35 +0100

    Adding Books Route Selenium Tests
    - Manage_books_actions_test.py - Created and deletes a book as a privileged user

--- test/html_test/book_view/Manage_books_actions_test.py
+++ test/html_test/book_view/Manage_books_actions_test.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
 
-class AddBookPage(unittest.TestCase):
+class ManageBooksActionsTest(unittest.TestCase):
     """
         Tests flow of navigation to add new book.
         Simulates users Login
@@ -18,30 +18,33 @@ class AddBookPage(unittest.TestCase):
 
     def setUp(self):
         options = webdriver.FirefoxOptions()
-        options.add_argument('-headless')
+        options.add_argument('')
         self.driver = webdriver.Firefox(options=options)
 
     def tearDown(self):
         self.driver.close()
 
     def login(self):
-        self.driver.get("http://0.0.0.0:5000/login")
+        self.driver.get("https://the-bookshelf-milestone-three.herokuapp.com/login")
         username_tested = self.driver.find_element_by_id("username")
         username_tested.send_keys("willfarnaby")
         password_tested = self.driver.find_element_by_id("password")
         password_tested.send_keys("123")
         self.driver.find_element_by_tag_name('button').click()
 
-    def test_add_without_login_session_redirects_to_login(self):
-        self.driver.get("http://0.0.0.0:5000/book/new")
+    def test_login_required_for_create_book_page(self):
+        """
+            User should be redirected to login page if not authenticated
+        """
+        self.driver.get("https://the-bookshelf-milestone-three.herokuapp.com/book/new")
         WebDriverWait(self.driver, 3).until(
             expected_conditions.text_to_be_present_in_element(
                 (By.TAG_NAME, "h4"), "Login to execute this operation"),
         )
 
-    def test_add_delete_book(self):
+    def test_add_book(self):
         """
-            Open add new book form, Create new book, delete book
+            Open new book form, fills fields for new book, submits form
         """
         self.login()
         # Navigates to Add new book form page
@@ -70,12 +73,26 @@ class AddBookPage(unittest.TestCase):
                 (By.TAG_NAME, "h4"), "New Book added!")
         )
 
-        # Delete book
-        self.driver.find_element_by_tag_name('button').click()
-        WebDriverWait(self.driver, 3).until(
+    def test_delete_book(self):
+        """
+            Search for book, navigate to book_page, use page buttons to remove book
+        """
+        # needs to be logged in to remove a book
+        self.login()
+        # search for new book
+        title = self.driver.find_element_by_id('query')
+        title.send_keys('new book')
+        # navigate to book page
+        self.driver.find_element_by_link_text('Book Detailsinfo_outline').click()
+
+        self.driver.find_element_by_link_text('ACTIONS\narrow_drop_down').click()
+        buttons = self.driver.find_elements_by_tag_name('button')
+        buttons[1].click()
+        WebDriverWait(self.driver, 5).until(
             expected_conditions.text_to_be_present_in_element(
                 (By.TAG_NAME, "h4"), "Book Removed")
         )
+
 
 if __name__ == "__main__":
     unittest.main()
